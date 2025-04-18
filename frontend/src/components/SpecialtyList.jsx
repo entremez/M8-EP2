1c63c302ff283cb01c2ff46efa28b0b0fa3e6031
entremez <mespinozazuarez@gmail.com>
2025-04-18 11:07:48 -0400

    Add some changes to develop branch

--- frontend/src/components/SpecialtyList.jsx
+++ frontend/src/components/SpecialtyList.jsx
@@ -23,7 +23,6 @@ const SpecialtyList = () => {
 
   return (
     <div className="p-4">
-      <h2 className="text-xl font-bold mb-2">Especialidades:</h2>
       {error && <p className="text-red-500">{error}</p>}
       <ul className="list-disc pl-5">
         {specialties.map((spec, idx) => (
